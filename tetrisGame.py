46d8df3fae7ad5de58ac87e741c2d1c9fd454dc5
Ryan Koeninger <rkoeninger2003@gmail.com>
2025-04-11 22:50:10 -0700

    aaaaa

--- tetrisGame.py
+++ tetrisGame.py
@@ -6,7 +6,9 @@ BLOCK_TYPES = ['O', 'L', 'J', 'I', 'T', 'Z', 'S']
 
 # while playing i have a game tick set up, and gravity will be 
 # move down if ticks % gravity == 0; higher gravity is slower 
-GRAVITY = 10000
+GRAVITY = 10
+
+REPLAY = True
 
 BLOCKS = {
     "O": [
@@ -176,6 +178,12 @@ class Block:
         num_rotations = len(BLOCKS[self.type])
         self.rotation = (self.rotation + x) % num_rotations
         self.block = BLOCKS[self.type][self.rotation]
+    
+    def clone(self):
+        new_block = Block(self.type, self.rotation)
+        new_block.x = self.x
+        new_block.y = self.y
+        return new_block
             
     
 
@@ -184,8 +192,8 @@ class GameBoard:
     def __init__(self, height = 20, width = 10, padding = 4): # initialized to standard tetris board size plus padding
         self.height = height
         self.width = width 
-        self.paddedHeight = height + padding
-        self.paddedWidth = width + padding
+        self.hidden = padding
+        self.totalHeight = height + padding
 
         self.board = [] # board will be represented as 2d array (matrix)
         self.activeBoard = [] # this is the temporary board where we do the block rotation actions and stuff and check bounds
@@ -193,20 +201,21 @@ class GameBoard:
         self.blocks = BLOCK_TYPES[:]
         self.active = False # for determining which board to show
 
-        for _ in range(height):
+        for _ in range(self.totalHeight):
             row = [0] * width
             self.board.append(row)
 
-        for _ in range(self.paddedHeight):
-            rows = [0] * (self.paddedWidth)
+        for _ in range(self.totalHeight):
+            rows = [0] * (self.width)
             self.activeBoard.append(rows)
 
             
     def __str__(self):
+        visible_board = self.board[self.hidden:]  # skip the top 'padding' rows
         if self.active: 
-            return "\n".join(" ".join(str(cell) for cell in row) for row in self.activeBoard) 
+            return "\n".join(" ".join(str(cell) for cell in row) for row in self.activeBoard[self.hidden:])
         else: 
-            return "\n".join(" ".join(str(cell) for cell in row) for row in self.board)
+            return "\n".join(" ".join(str(cell) for cell in row) for row in visible_board)
 
     def newBlock(self): 
         if self.blocks == []:
@@ -214,37 +223,34 @@ class GameBoard:
         random.shuffle(self.blocks)
         self.currentBlock = Block(self.blocks[0])
         self.blocks.pop(0)
+        self.currentBlock.y = self.hidden
         self.drawBlock()
         
-# GOAL: every move call this on all rows, and depending on how many rows return True increase score accordingly
-    def checkClear(self):
-        rowsCleared = 0
-        for row in self.board:
-            isFull = all(val == 1 for val in row) # checks if the whole row is 1s (full row)
-            if isFull: 
-                rowsCleared += 1
+    def clearRows(self):
+        visible_board = self.board[self.hidden:]
+        updated_visible = [row for row in visible_board if not all(cell == 1 for cell in row)]
+        rowsCleared = len(visible_board) - len(updated_visible)
+        
+
+        for _ in range(rowsCleared):
+            updated_visible.insert(0, [0] * self.width)
+
+        self.board = self.board[:self.hidden] + updated_visible
         return rowsCleared
     
-    def clearRows(self, clearedCount):
-        updatedBoard = []
-
-        # currently this clears the row but also clears the whole board for some reason and the size of the board gets messed up
-        for i in range(self.height - 1 - clearedCount): 
-            updatedBoard.append(self.board[i])
+    def checkTop(self):    
+        for cell in self.board[self.hidden]:
+            if cell != 0:
+                return True
+        return False
         
-        for _ in range(clearedCount): 
-            updatedBoard.insert(0, [0] * self.width)
-        
-        self.board = updatedBoard
-        
-
         
     # input current block x and y + 1 in whichever direction you want to move
-    def canMove(self, newX, newY):
+    def canMove(self, block, newX, newY):
         # for loops figure out where the block actually is
         for y in range(4):
             for x in range(4):
-                if self.currentBlock.block[y][x]: 
+                if block.block[y][x]: 
                     # need to adjust for where the 1 actually is in the 4x4 grid
                     boardX = newX + x
                     boardY = newY + y 
@@ -252,10 +258,10 @@ class GameBoard:
                     # print('y: ' + str(boardY))
 
                     # check boundaries
-                    if boardX < 0 or boardX >= 10:
+                    if boardX < 0 or boardX >= self.width:
                         print('out of bounds')
                         return False
-                    if boardY < 0 or boardY >= 20:
+                    if boardY < 0 or boardY >= self.totalHeight:
                         print('at the top/bottom')
                         return False
                     
@@ -274,7 +280,7 @@ class GameBoard:
                 if self.currentBlock.block[i][j]:
                     x = self.currentBlock.x + j
                     y = self.currentBlock.y + i
-                    if 0 <= y < self.height and 0 <= x < self.width:
+                    if 0 <= y < self.totalHeight and 0 <= x < self.width:
                         self.activeBoard[y][x] = 1
 
     
@@ -289,7 +295,8 @@ class GameBoard:
                 if self.currentBlock.block[j][i]:
                     boardX = self.currentBlock.x + i
                     boardY = self.currentBlock.y + j
-                    self.board[boardY][boardX] = 1
+                    if 0 <= boardY < self.totalHeight and 0 <= boardX < self.width:
+                        self.board[boardY][boardX] = 1
         self.currentBlock = None
 
 
@@ -301,23 +308,25 @@ class Tetris(GameBoard):
 
     def increaseScore(self, value):
         self.score += value
-
+    
     def moveRight(self):
-        if self.canMove(self.currentBlock.x + 1, self.currentBlock.y):
+        if self.canMove(self.currentBlock, self.currentBlock.x + 1, self.currentBlock.y):
             self.currentBlock.x += 1
             self.drawBlock()
         else: 
             print('cant move right')
 
     def moveLeft(self):
-        if self.canMove(self.currentBlock.x - 1, self.currentBlock.y):
+        if self.canMove(self.currentBlock, self.currentBlock.x - 1, self.currentBlock.y):
             self.currentBlock.x -= 1
             self.drawBlock()
         else: 
             print('cant move left')
 
     def moveDown(self):
-        if self.canMove(self.currentBlock.x, self.currentBlock.y + 1):
+        if self.currentBlock is None:
+            return False
+        if self.canMove(self.currentBlock, self.currentBlock.x, self.currentBlock.y + 1):
             self.currentBlock.y +=1
             self.drawBlock()
         else: 
@@ -326,31 +335,64 @@ class Tetris(GameBoard):
             return False
 
     def rotateLeft(self):
-        newConfig = self.currentBlock
+        newConfig = self.currentBlock.clone()
         newConfig.rotate(3)
-        if self.canMove(newConfig.x, newConfig.y):
-            self.currentBlock = newConfig
-            self.drawBlock()
-        else: 
-            print('cant rotate left')
+
+        candidate_offsets = [0, -1, 1, -2, 2]
+
+        for offset in candidate_offsets:
+            tempX = newConfig.x + offset
+            
+            if self.canMove(newConfig, tempX, newConfig.y):
+                newConfig.x = tempX
+                self.currentBlock = newConfig
+                self.drawBlock()
+                return
+        print('cant rotate left')
 
     def rotateRight(self):
-        newConfig = self.currentBlock
+        newConfig = self.currentBlock.clone()
         newConfig.rotate(1)
-        if self.canMove(newConfig.x, newConfig.y):
-            self.currentBlock = newConfig
-            self.drawBlock()
-        else: 
-            print('cant rotate right')
+
+        candidate_offsets = [0, -1, 1, -2, 2]
+
+        for offset in candidate_offsets:
+            tempX = newConfig.x + offset
+            if self.canMove(newConfig, tempX, newConfig.y):
+                newConfig.x = tempX
+                self.currentBlock = newConfig
+                self.drawBlock()
+                return
+        print('cant rotate right')
 
     def rotateFlip(self):
-        newConfig = self.currentBlock
+        newConfig = self.currentBlock.clone()
         newConfig.rotate(2)
-        if self.canMove(newConfig.x, newConfig.y):
-            self.currentBlock = newConfig
-            self.drawBlock()
-        else: 
-            print('cant rotate 180')
+
+        candidate_offsets = [0, -1, 1, -2, 2]
+
+        for offset in candidate_offsets:
+            tempX = newConfig.x + offset
+            if self.canMove(newConfig, tempX, newConfig.y):
+                newConfig.x = tempX
+                self.currentBlock = newConfig
+                self.drawBlock()
+                return
+        print("cant rotate 180")
+
+
+def game_over_screen(stdscr, score):
+    stdscr.clear()
+    screen_height, screen_width = stdscr.getmaxyx()
+    msg1 = f"SCORE: {score}"
+    msg2 = "Y: PLAY AGAIN, ANY KEY TO EXIT"
+    time.sleep(0.2)
+    stdscr.addstr(screen_height // 2 - 1, (screen_width - len(msg1)) // 2, msg1)
+    stdscr.addstr(screen_height // 2, (screen_width - len(msg2)) // 2, msg2)
+    stdscr.refresh()
+    key = stdscr.getch()
+    return key == ord('y')
+
 
 # currently formatted for playing in terminal
 # stdscr represents the terminal window
@@ -359,65 +401,86 @@ def playGame(stdscr):
     stdscr.nodelay(True) # doesn't wait for keypress to do calls
     stdscr.timeout(100) # refresh rate 100ms
 
-    playing = True
-    game = Tetris()
-    score = 0
-
-    ticks = 0
-    while playing: 
-        stdscr.clear() # refresh screen
-
-        if game.currentBlock == None:
-            # TODO: CHECK FOR FULL LINES AND CLEAR THEM HERE
-            game.newBlock()
-
-        # this is which board we are drawing
-        board_str = game.__str__().split('\n')
-
-        # OPERATIONS BELOW TO CENTER IT IN THE TERMINAL
-        # get terminal size
-        screen_height, screen_width = stdscr.getmaxyx()
-        # get board size
-        board_height = len(board_str)
-        board_width = len(board_str[0]) if board_str else 0
-        # calculate starting y,x to center
-        start_y = max((screen_height - board_height) // 2, 0)
-        start_x = max((screen_width - board_width) // 2, 0)
-
-        # draw board on terminal
-        for i, line in enumerate(board_str):
-            stdscr.addstr(start_y + i, start_x, line)
-        stdscr.refresh()
-
-        try:
-           key = stdscr.getch() 
-        except:
-            key = -1
+    while True:
+        playing = True
+        game = Tetris()
+        score = 0
+        currentCombo = 0
+        ticks = 0
         
-        if key == curses.KEY_LEFT:
-            game.moveLeft()
-        elif key == curses.KEY_RIGHT:
-            game.moveRight()
-        elif key == curses.KEY_DOWN:
-            game.moveDown()
-        elif key == ord('a'):
-            game.rotateLeft()
-        elif key == ord('s'):
-            game.rotateFlip()
-        elif key == ord('d'):
-            game.rotateRight()
+    # how many pieces placed in a row that have cleared at least one row
         
-        rowsCleared = game.checkClear()
-        if rowsCleared > 0:
-            game.increaseScore(score)
-            game.clearRows(rowsCleared)
 
-        if ticks % GRAVITY == 0:
-            game.moveDown()
+        while playing: 
+            
+            stdscr.clear() # refresh screen
 
-        
-        
-        time.sleep(0.05) # doesn't loop too fast
+            rowsCleared = game.clearRows()
+            if rowsCleared == 0:
+                currentCombo = 0
+            else:
+                currentCombo += 1
+            
+            if game.checkTop():
+                playing = False
+                break
+
+            # basic system that attempts to increase scoring for bigger clears and longer combos
+            score += (rowsCleared + currentCombo) ** 2 
+            
+            if game.currentBlock == None:
+                # TODO: CHECK FOR FULL LINES AND CLEAR THEM HERE
+                game.newBlock()
+
+            # this is which board we are drawing
+            board_str = game.__str__().split('\n')
+
+            # OPERATIONS BELOW TO CENTER IT IN THE TERMINAL
+            # get terminal size
+            screen_height, screen_width = stdscr.getmaxyx()
+            # get board size
+            board_height = len(board_str)
+            board_width = len(board_str[0]) if board_str else 0
+            # calculate starting y,x to center
+            start_y = max((screen_height - board_height) // 2, 0)
+            start_x = max((screen_width - board_width) // 2, 0)
+
+            # draw board on terminal
+            for i, line in enumerate(board_str):
+                stdscr.addstr(start_y + i, start_x, line)
+            stdscr.refresh()
+
+            try:
+                key = stdscr.getch() 
+            except:
+                key = -1
+            
+            if key == curses.KEY_LEFT:
+                game.moveLeft()
+            elif key == curses.KEY_RIGHT:
+                game.moveRight()
+            elif key == curses.KEY_DOWN:
+                game.moveDown()
+            elif key == ord('a'):
+                game.rotateLeft()
+            elif key == ord('s'):
+                game.rotateFlip()
+            elif key == ord('d'):
+                game.rotateRight()
+            
+            # we can stop the game now
+            elif key == ord('p'):
+                game_over_screen(stdscr, score)
+
+            ticks += 1
+            if ticks % GRAVITY == 0:
+                game.moveDown()
+
+            time.sleep(0.05) # doesn't loop too fast
+        if game_over_screen(stdscr, score):
+            continue
+        else:
+            break
 
 
 curses.wrapper(playGame)
